1e4a7af8627e81532bee4207ca2aa2b237a6cd1b
Shir Bar Lev <shirblc@gmail.com>
2020-11-10 16:19:33 +0000

    Added Book component (displaying a single book).

---
+++ src/Book.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+// Single book component
+export class Book extends Component {
+	// render method
+	render() {
+		return (
+			 <li key={this.props.key}>
+				<div className="book">
+				  <div className="book-top">
+					<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("{this.props.book.coverUrl}")' }}></div>
+					<div className="book-shelf-changer">
+					  <select>
+						<option value="move" disabled>Move to...</option>
+						<option value="currentlyReading">Currently Reading</option>
+						<option value="wantToRead">Want to Read</option>
+						<option value="read">Read</option>
+						<option value="none">None</option>
+					  </select>
+					</div>
+				  </div>
+				  <div className="book-title">{this.props.book.title}</div>
+				  <div className="book-authors">{this.props.book.author}</div>
+				</div>
+			  </li>
+		)
+	}
+}
