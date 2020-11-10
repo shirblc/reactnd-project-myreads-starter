3a9913c04b4767c7e95fdce377c8285b61fe4736
Shir Bar Lev <shirblc@gmail.com>
2020-11-10 16:42:13 +0000

    Added shelf handling in the Book component.

--- src/Book.js
+++ src/Book.js
@@ -2,6 +2,17 @@ import React from 'react';
 
 // Single book component
 class Book extends React.Component {
+	state = {
+		currentShelf: this.props.currentShelf
+	}
+
+	// update the shelf in which the book should be displayed
+	updateShelf = (value) => {
+		this.setState({
+			currentShelf: value
+		});
+	}
+	
 	// render method
 	render() {
 		return (
@@ -10,7 +21,7 @@ class Book extends React.Component {
 				  <div className="book-top">
 					<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("{this.props.coverUrl}")' }}></div>
 					<div className="book-shelf-changer">
-					  <select>
+					  <select value={this.state.currentShelf} onChange={(event) => (this.updateShelf(event.target.value))}>
 						<option value="move" disabled>Move to...</option>
 						<option value="currentlyReading">Currently Reading</option>
 						<option value="wantToRead">Want to Read</option>
