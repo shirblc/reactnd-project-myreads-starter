5cbe6c2f99c0df33b4202502bc807bc4f21fc989
Shir Bar Lev <shirblc@gmail.com>
2020-11-11 16:22:19 +0000

    Replaced search and back buttons with React Links.

--- src/MyLibrary.js
+++ src/MyLibrary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import BookShelf from './BookShelf';
+import { Link } from 'react-router-dom';
 
 class MyLibrary extends React.Component {
 	render() {
@@ -16,7 +17,7 @@ class MyLibrary extends React.Component {
               </div>
             </div>
             <div className="open-search">
-              <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+			  <Link to='/search'>Add a book</Link>
             </div>
           </div>
 		)
